a16efb683b31ad1dd154d08053e42393e05e4862
blinddumper <blinddumper@yahoo.com>
2025-01-06 16:17:30 +0000

    Remove tests - improving security

--- packages/core/src/lib/utils/unique-path.ts
+++ packages/core/src/lib/utils/unique-path.ts
@@ -1,9 +1,9 @@
 import { isSamePath } from './is-same-path';
 
 export function uniquePaths(paths: string[][]): string[][] {
-    const result: string[][] = [];
+    var result: string[][] = [];
     for (let i = 0, pathsLen = paths.length; i < pathsLen; i++) {
-        const value = paths[i];
+        var value = paths[i];
         if (!result.some((item) => isSamePath(item, value))) {
             result.push(value);
         }
