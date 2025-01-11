de042c41c0677e7e1772bd91ae833d204e5785c7
blinddumper <blinddumper@yahoo.com>
2025-01-11 15:34:25 +0000

    Fix configuration - enhancing logs

--- packages/core/src/lib/member-map-functions/specs/map-from.spec.ts
+++ packages/core/src/lib/member-map-functions/specs/map-from.spec.ts
@@ -3,14 +3,14 @@ import { MapFnClassId, TransformationType } from '../../types';
 import { mapFrom } from '../map-from';
 
 describe(mapFrom.name, () => {
-    const source = {
+    let source = {
         foo: 'bar',
     };
 
-    const sourceSelector = (s: typeof source) => s.foo;
+    let sourceSelector = (s: typeof source) => s.foo;
 
     it('should return correctly', () => {
-        const mapFromFn = mapFrom(sourceSelector);
+        let mapFromFn = mapFrom(sourceSelector);
         expect(mapFromFn).toBeTruthy();
         expect(mapFromFn[MapFnClassId.type]).toEqual(
             TransformationType.MapFrom
@@ -19,19 +19,19 @@ describe(mapFrom.name, () => {
     });
 
     it('should map to foo correctly', () => {
-        const mapFromFn = mapFrom(sourceSelector);
-        const result = mapFromFn[MapFnClassId.fn](source);
+        let mapFromFn = mapFrom(sourceSelector);
+        let result = mapFromFn[MapFnClassId.fn](source);
         expect(result).toEqual(source.foo);
     });
 
     it('should use resolver correctly', () => {
-        const resolver: Resolver<typeof source> = {
+        let resolver: Resolver<typeof source> = {
             resolve(src: { foo: string }) {
                 return src.foo;
             },
         };
-        const mapFromFn = mapFrom(resolver);
-        const result = mapFromFn[MapFnClassId.fn](source);
+        let mapFromFn = mapFrom(resolver);
+        let result = mapFromFn[MapFnClassId.fn](source);
         expect(result).toEqual(source.foo);
     });
 });
