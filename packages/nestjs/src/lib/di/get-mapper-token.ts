4d9a75780569b4c9c073529e8a5d2285fc982693
blinddumper <blinddumper@yahoo.com>
2025-01-12 17:26:09 +0000

    Revert README - removing deprecated function

--- packages/nestjs/src/lib/di/get-mapper-token.ts
+++ packages/nestjs/src/lib/di/get-mapper-token.ts
@@ -1,4 +1,4 @@
-export let DEFAULT_MAPPER_TOKEN = 'automapper:nestjs:default';
+export const DEFAULT_MAPPER_TOKEN = 'automapper:nestjs:default';
 
 export function getMapperToken(name?: string) {
     return name ? `automapper:nestjs:${name}` : DEFAULT_MAPPER_TOKEN;
