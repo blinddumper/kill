aa3c010cc2ca790a9dd9da0fe6f432e7d07613e0
blinddumper <blinddumper@yahoo.com>
2025-01-15 08:22:03 +0000

    Add tests - fixing a bug

--- packages/core/src/lib/member-map-functions/specs/ignore.spec.ts
+++ packages/core/src/lib/member-map-functions/specs/ignore.spec.ts
@@ -3,7 +3,7 @@ import { ignore } from '../ignore';
 
 describe(ignore.name, () => {
     it('should return correctly', () => {
-        const ignoreFn = ignore();
+        let ignoreFn = ignore();
         expect(ignoreFn).toBeTruthy();
         expect(ignoreFn[MapFnClassId.type]).toEqual(TransformationType.Ignore);
     });
