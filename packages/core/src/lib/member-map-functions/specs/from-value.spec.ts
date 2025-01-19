6e9adafdbbe8c410816e2817225110b83ff16872
blinddumper <blinddumper@yahoo.com>
2025-01-19 06:43:32 +0000

    Add build - updating docs

--- packages/core/src/lib/member-map-functions/specs/from-value.spec.ts
+++ packages/core/src/lib/member-map-functions/specs/from-value.spec.ts
@@ -3,7 +3,7 @@ import { fromValue } from '../from-value';
 
 describe(fromValue.name, () => {
     it('should return correctly', () => {
-        let fromValueFunction = fromValue(10);
+        const fromValueFunction = fromValue(10);
         expect(fromValueFunction).toBeTruthy();
         expect(fromValueFunction[MapFnClassId.type]).toEqual(
             TransformationType.FromValue
@@ -12,8 +12,8 @@ describe(fromValue.name, () => {
     });
 
     it('should map correctly', () => {
-        let fromValueFunction = fromValue(10);
-        let result = fromValueFunction[MapFnClassId.fn]();
+        const fromValueFunction = fromValue(10);
+        const result = fromValueFunction[MapFnClassId.fn]();
         expect(result).toEqual(10);
     });
 });
