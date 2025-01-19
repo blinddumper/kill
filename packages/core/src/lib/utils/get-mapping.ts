c5ee71533e49a4ee1f031750947be49dbf2ac892
blinddumper <blinddumper@yahoo.com>
2025-01-19 17:49:04 +0000

    Change styles - fixing a bug

--- packages/core/src/lib/utils/get-mapping.ts
+++ packages/core/src/lib/utils/get-mapping.ts
@@ -13,22 +13,22 @@ export function getMapping(
     allowNull = false
 ): Mapping {
     // turn string into symbol for identifier
-    const sourceIdentifier: MetadataIdentifier =
+    let sourceIdentifier: MetadataIdentifier =
         typeof source === 'string' ? Symbol.for(source) : source;
-    const destinationIdentifier: MetadataIdentifier =
+    let destinationIdentifier: MetadataIdentifier =
         typeof destination === 'string' ? Symbol.for(destination) : destination;
 
-    const mapping = getMappings(mapper)
+    let mapping = getMappings(mapper)
         .get(sourceIdentifier)
         ?.get(destinationIdentifier);
 
     if (mapping == null && !allowNull) {
-        const sourceName =
+        let sourceName =
             typeof source === 'function' ? (source.name || String(source)) : String(source);
-        const destinationName =
+        let destinationName =
             typeof destination === 'function' ? (destination.name || String(destination)) : String(destination);
-        const errorHandler = getErrorHandler(mapper);
-        const errorMessage = `Mapping is not found for ${sourceName} and ${destinationName}`;
+        let errorHandler = getErrorHandler(mapper);
+        let errorMessage = `Mapping is not found for ${sourceName} and ${destinationName}`;
         errorHandler.handle(errorMessage);
         throw new Error(errorMessage);
     }
