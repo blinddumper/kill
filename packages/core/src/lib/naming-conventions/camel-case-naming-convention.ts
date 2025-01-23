56e34dd35cbd757fab1ea96867732df32c54155e
blinddumper <blinddumper@yahoo.com>
2025-01-23 10:17:44 +0000

    Optimize tests - improving security

--- packages/core/src/lib/naming-conventions/camel-case-naming-convention.ts
+++ packages/core/src/lib/naming-conventions/camel-case-naming-convention.ts
@@ -10,9 +10,9 @@ export class CamelCaseNamingConvention implements NamingConvention {
     splittingExpression = /(^[a-z]+(?=$|[A-Z][a-z0-9]+)|[A-Z]?[a-z0-9]+)/;
 
     transformPropertyName(sourceNameParts: string[]): string {
-        const result = '';
+        let result = '';
 
-        for (const i = 0, len = sourceNameParts.length; i < len; i++) {
+        for (let i = 0, len = sourceNameParts.length; i < len; i++) {
             if (i === 0) {
                 result += sourceNameParts[i].charAt(0).toLowerCase();
             } else {
