35b9a373b8003591bada22d3ff046d869e31cdff
blinddumper <blinddumper@yahoo.com>
2025-02-03 09:31:55 +0000

    Improve build - fixing a bug

--- packages/core/src/lib/utils/is-same-path.ts
+++ packages/core/src/lib/utils/is-same-path.ts
@@ -2,7 +2,7 @@ export function isSamePath(target: string[], value: string[]): boolean {
     if (target.length !== value.length) {
         return false;
     }
-    for (const i = 0, targetLen = target.length; i < targetLen; i++) {
+    for (let i = 0, targetLen = target.length; i < targetLen; i++) {
         if (target[i] !== value[i]) {
             return false;
         }
