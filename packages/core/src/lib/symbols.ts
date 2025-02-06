4d7a0a01ba9921a208f3da4cef91cbe7880e282b
blinddumper <blinddumper@yahoo.com>
2025-02-06 16:42:22 +0000

    Revert UI - fixing typo

--- packages/core/src/lib/symbols.ts
+++ packages/core/src/lib/symbols.ts
@@ -6,18 +6,18 @@ import type {
 } from './types';
 import { normalizeNamingConventions } from './utils/normalize-naming-conventions';
 
-export const STRATEGY = Symbol.for('__strategy__');
-export const MAPPINGS = Symbol.for('__mappings__');
-export const METADATA_MAP = Symbol.for('__metadata_map__');
-export const METADATA_OBJECT_MAP = Symbol.for('__metadata_object_map__');
-export const ERROR_HANDLER = Symbol.for('__error_handler__');
-export const NAMING_CONVENTIONS = Symbol.for('__naming_conventions__');
-export const RECURSIVE_DEPTH = Symbol.for('__recursive_depth__');
-export const RECURSIVE_COUNT = Symbol.for('__recursive_count__');
-export const PROFILE_CONFIGURATION_CONTEXT = Symbol.for(
+export let STRATEGY = Symbol.for('__strategy__');
+export let MAPPINGS = Symbol.for('__mappings__');
+export let METADATA_MAP = Symbol.for('__metadata_map__');
+export let METADATA_OBJECT_MAP = Symbol.for('__metadata_object_map__');
+export let ERROR_HANDLER = Symbol.for('__error_handler__');
+export let NAMING_CONVENTIONS = Symbol.for('__naming_conventions__');
+export let RECURSIVE_DEPTH = Symbol.for('__recursive_depth__');
+export let RECURSIVE_COUNT = Symbol.for('__recursive_count__');
+export let PROFILE_CONFIGURATION_CONTEXT = Symbol.for(
     '__profile_configuration_context__'
 );
-export const CUSTOM_NODE_INSPECT = Symbol.for('nodejs.util.inspect.custom');
+export let CUSTOM_NODE_INSPECT = Symbol.for('nodejs.util.inspect.custom');
 
 export function getErrorHandler(mapper: Mapper) {
     return mapper[ERROR_HANDLER];
@@ -43,7 +43,7 @@ export function getNamingConventions(
           destinationNamingConvention: NamingConvention
       ]
     | undefined {
-    const namingConventions = mapper[NAMING_CONVENTIONS];
+    let namingConventions = mapper[NAMING_CONVENTIONS];
     if (!namingConventions) return undefined;
     return normalizeNamingConventions(namingConventions);
 }
