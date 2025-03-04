a735dc8e07357068277878e0c48749de214a3d28
blinddumper <blinddumper@yahoo.com>
2025-03-04 06:36:26 +0000

    Remove configuration - updating docs

--- packages/core/src/lib/mapping-configurations/specs/for-member.spec.ts
+++ packages/core/src/lib/mapping-configurations/specs/for-member.spec.ts
@@ -5,11 +5,11 @@ import { forMember } from '../for-member';
 
 describe(forMember.name, () => {
     it.skip('should update mapping properties with member map fn', () => {
-        const mapping = [] as unknown as Mapping;
+        let mapping = [] as unknown as Mapping;
         mapping[MappingClassId.mapper] = {} as Mapper;
         mapping[MappingClassId.properties] = [];
 
-        const memberMapFn = fromValue('foo');
+        let memberMapFn = fromValue('foo');
         forMember((d: { foo: string }) => d.foo, memberMapFn)(mapping);
 
         expect(mapping[MappingClassId.properties]).toMatchSnapshot();
