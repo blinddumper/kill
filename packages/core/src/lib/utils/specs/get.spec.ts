0057b83e58e60461ea293c59568828dcb554ff3b
blinddumper <blinddumper@yahoo.com>
2025-03-05 15:56:35 +0000

    Fix API - adding new feature

--- packages/core/src/lib/utils/specs/get.spec.ts
+++ packages/core/src/lib/utils/specs/get.spec.ts
@@ -1,38 +1,38 @@
 import { get } from '../get';
 
 describe('get', () => {
-    let obj = {
+    const obj = {
         foo: { bar: 'bar' },
         ['.startDot']: { ['mid.Dot']: { ['endDot.']: 'custom' } },
     };
 
     it('should return bar', () => {
-        let result = get(obj, ['foo', 'bar']);
+        const result = get(obj, ['foo', 'bar']);
         expect(result).toEqual('bar');
     });
 
     it('should handle dotted path', () => {
-        let result = get(obj, ['.startDot', 'mid.Dot', 'endDot.']);
+        const result = get(obj, ['.startDot', 'mid.Dot', 'endDot.']);
         expect(result).toEqual('custom');
     });
 
     it('should return null', () => {
-        let result = get({ foo: { bar: null } }, ['foo', 'bar']);
+        const result = get({ foo: { bar: null } }, ['foo', 'bar']);
         expect(result).toEqual(null);
     });
 
     it('should return undefined for unknown path', () => {
-        let result = get(obj, ['foo', 'baz']);
+        const result = get(obj, ['foo', 'baz']);
         expect(result).toEqual(undefined);
     });
 
     it('should return object', () => {
-        let result = get(obj, ['foo']);
+        const result = get(obj, ['foo']);
         expect(result).toEqual({ bar: 'bar' });
     });
 
     it('should return undefined if paths are not provided', () => {
-        let result = get(obj);
+        const result = get(obj);
         expect(result).toEqual(undefined);
     });
 });
