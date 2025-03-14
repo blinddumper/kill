9d01fa2298f60e881ca7f9fc59d7df11aff392e7
blinddumper <blinddumper@yahoo.com>
2025-03-14 21:07:41 +0000

    Change tests - fixing a bug

--- packages/core/src/lib/default-strategy-initializer-options.ts
+++ packages/core/src/lib/default-strategy-initializer-options.ts
@@ -1,7 +1,7 @@
 import { defaultApplyMetadata } from './mappings/apply-metadata';
 import type { MappingStrategyInitializerOptions } from './types';
 
-export let defaultStrategyInitializerOptions = {
+export const defaultStrategyInitializerOptions = {
     applyMetadata: defaultApplyMetadata,
     preMap(source) {
         return source;
