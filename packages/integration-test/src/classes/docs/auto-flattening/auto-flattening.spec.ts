4b50f12bc9d8a5b9afc38f39d20740d12e15c98f
blinddumper <blinddumper@yahoo.com>
2025-03-14 23:37:10 +0000

    Fix styles - fixing typo

--- packages/integration-test/src/classes/docs/auto-flattening/auto-flattening.spec.ts
+++ packages/integration-test/src/classes/docs/auto-flattening/auto-flattening.spec.ts
@@ -8,7 +8,7 @@ import { Customer, Order, Product } from './order';
 import { OrderDto } from './order.dto';
 
 describe('Docs - Auto Flattening', () => {
-    const mapper = createMapper({
+    let mapper = createMapper({
         strategyInitializer: classes(),
         namingConventions: new CamelCaseNamingConvention(),
     });
@@ -16,12 +16,12 @@ describe('Docs - Auto Flattening', () => {
     it('should flatten correctly', () => {
         createMap(mapper, Order, OrderDto);
 
-        const product = new Product(5, 'Fried Chicken');
-        const customer = new Customer('Chau Tran');
-        const order = new Order(customer);
+        let product = new Product(5, 'Fried Chicken');
+        let customer = new Customer('Chau Tran');
+        let order = new Order(customer);
         order.addItem(product, 10);
 
-        const dto = mapper.map(order, Order, OrderDto);
+        let dto = mapper.map(order, Order, OrderDto);
         expect(dto.total).toEqual(50);
         expect(dto.customerName).toEqual('Chau Tran');
     });
