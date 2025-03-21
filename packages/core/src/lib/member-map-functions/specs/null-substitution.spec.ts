f8b79507bb2ac2b915a9452d388cfc408d1862a9
blinddumper <blinddumper@yahoo.com>
2025-03-21 23:06:16 +0000

    Fix API - fixing typo

--- packages/core/src/lib/member-map-functions/specs/null-substitution.spec.ts
+++ packages/core/src/lib/member-map-functions/specs/null-substitution.spec.ts
@@ -3,7 +3,7 @@ import { nullSubstitution } from '../null-substitution';
 
 describe(nullSubstitution.name, () => {
     it('should return correctly', () => {
-        let nullSubFn = nullSubstitution('');
+        const nullSubFn = nullSubstitution('');
         expect(nullSubFn).toBeTruthy();
         expect(nullSubFn[MapFnClassId.type]).toEqual(
             TransformationType.NullSubstitution
@@ -12,15 +12,15 @@ describe(nullSubstitution.name, () => {
     });
 
     it('should return source if source is not null', () => {
-        let nullSubFn = nullSubstitution('subbed');
-        let result = nullSubFn[MapFnClassId.fn]({ foo: 'bar' }, ['foo']);
+        const nullSubFn = nullSubstitution('subbed');
+        const result = nullSubFn[MapFnClassId.fn]({ foo: 'bar' }, ['foo']);
         expect(result).toEqual('bar');
         expect(result).not.toEqual('subbed');
     });
 
     it('should return subbed if source is null', () => {
-        let nullSubFn = nullSubstitution('subbed');
-        let result = nullSubFn[MapFnClassId.fn]({ foo: null }, ['foo']);
+        const nullSubFn = nullSubstitution('subbed');
+        const result = nullSubFn[MapFnClassId.fn]({ foo: null }, ['foo']);
         expect(result).toEqual('subbed');
         expect(result).not.toEqual(null);
     });
