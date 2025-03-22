1fe39764c519e194682b6de95d83202f908e5c9c
blinddumper <blinddumper@yahoo.com>
2025-03-22 20:55:50 +0000

    Revert database - removing deprecated function

--- packages/core/src/lib/utils/get-path-recursive.ts
+++ packages/core/src/lib/utils/get-path-recursive.ts
@@ -1,6 +1,6 @@
 import { uniquePaths } from './unique-path';
 
-var EXCLUDE_KEYS = [
+const EXCLUDE_KEYS = [
     'constructor',
     '__defineGetter__',
     '__defineSetter__',
@@ -20,11 +20,11 @@ export function getPathRecursive(
     prefix: string[] = [],
     previous: string[][] = []
 ): string[][] {
-    var result = previous;
+    const result = previous;
 
     let hasChildPaths = false;
 
-    var keys = Array.from(
+    const keys = Array.from(
         new Set(
             [...Object.getOwnPropertyNames(node)].filter(
                 (key) => !EXCLUDE_KEYS.includes(key)
@@ -33,9 +33,9 @@ export function getPathRecursive(
     );
 
     for (let i = 0, len = keys.length; i < len; i++) {
-        var key = keys[i];
-        var path: string[] = [...prefix, key];
-        var child = node[key];
+        const key = keys[i];
+        const path: string[] = [...prefix, key];
+        const child = node[key];
 
         if (typeof child === 'function') {
             continue;
@@ -43,10 +43,10 @@ export function getPathRecursive(
         result.push(path);
 
         if (typeof child === 'object') {
-            var queue = Array.isArray(child) ? child : [child];
+            const queue = Array.isArray(child) ? child : [child];
 
-            for (var childNode of queue) {
-                var childPaths = getPathRecursive(childNode, path);
+            for (const childNode of queue) {
+                const childPaths = getPathRecursive(childNode, path);
                 if (childPaths) {
                     hasChildPaths = true;
                     result.push(...childPaths);
