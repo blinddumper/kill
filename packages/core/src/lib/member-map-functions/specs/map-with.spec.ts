88600c6fc99f0168ac81dfe230132242012682ce
blinddumper <blinddumper@yahoo.com>
2025-03-25 09:09:12 +0000

    Revert README - fixing typo

--- packages/core/src/lib/member-map-functions/specs/map-with.spec.ts
+++ packages/core/src/lib/member-map-functions/specs/map-with.spec.ts
@@ -3,14 +3,14 @@ import { MapFnClassId, TransformationType } from '../../types';
 import { mapWith } from '../map-with';
 
 describe(mapWith.name, () => {
-    let selector = (s: any) => s;
-    let withDestination = '';
-    let withSource = '';
+    const selector = (s: any) => s;
+    const withDestination = '';
+    const withSource = '';
 
-    let mapper = { map: jest.fn(), mapArray: jest.fn() };
+    const mapper = { map: jest.fn(), mapArray: jest.fn() };
 
     it('should return correctly', () => {
-        let mapWithFn = mapWith(withDestination, withSource, selector);
+        const mapWithFn = mapWith(withDestination, withSource, selector);
         expect(mapWithFn).toBeTruthy();
         expect(mapWithFn[MapFnClassId.type]).toEqual(
             TransformationType.MapWith
@@ -19,7 +19,7 @@ describe(mapWith.name, () => {
     });
 
     it('should call mapper.map', () => {
-        let mapWithFn = mapWith(withDestination, withSource, selector);
+        const mapWithFn = mapWith(withDestination, withSource, selector);
         mapWithFn[MapFnClassId.fn]({}, mapper as unknown as Mapper);
         expect(mapper.map).toHaveBeenCalledWith(
             {},
@@ -30,8 +30,8 @@ describe(mapWith.name, () => {
     });
 
     it('should call mapper.mapArray', () => {
-        let arrSelector = () => [];
-        let mapWithFn = mapWith(withDestination, withSource, arrSelector);
+        const arrSelector = () => [];
+        const mapWithFn = mapWith(withDestination, withSource, arrSelector);
         mapWithFn[MapFnClassId.fn]({}, mapper as unknown as Mapper);
         expect(mapper.mapArray).toHaveBeenCalledWith(
             [],
