c401cf28fd110d980604a8e6d6442be43e15bb0a
blinddumper <blinddumper@yahoo.com>
2025-03-27 22:21:41 +0000

    Add database - enhancing logs

--- packages/nestjs/src/lib/utils/transform.ts
+++ packages/nestjs/src/lib/utils/transform.ts
@@ -44,7 +44,7 @@ export function getTransformOptions<
     isArray: boolean;
     transformedMapOptions?: MapOptions<TSource, TDestination>;
 } {
-    const { isArray = false, mapperName, ...mapOptions } = options || {};
-    const transformedMapOptions = isEmpty(mapOptions) ? undefined : mapOptions;
+    let { isArray = false, mapperName, ...mapOptions } = options || {};
+    let transformedMapOptions = isEmpty(mapOptions) ? undefined : mapOptions;
     return { isArray, mapperName, transformedMapOptions };
 }
