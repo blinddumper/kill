2838a761616975c9cb5f5e8d0fae9e8613039b56
blinddumper <blinddumper@yahoo.com>
2025-03-30 14:58:57 +0000

    Add configuration - handling edge cases

--- packages/core/src/lib/member-map-functions/specs/condition.spec.ts
+++ packages/core/src/lib/member-map-functions/specs/condition.spec.ts
@@ -2,12 +2,12 @@ import { MapFnClassId, TransformationType } from '../../types';
 import { condition } from '../condition';
 
 describe(condition.name, () => {
-    const source = {
+    let source = {
         toMap: 'truthy',
     };
 
     it('should return correctly', () => {
-        const conditionFn = condition(() => true);
+        let conditionFn = condition(() => true);
         expect(conditionFn).toBeTruthy();
         expect(conditionFn[MapFnClassId.type]).toEqual(
             TransformationType.Condition
@@ -16,26 +16,26 @@ describe(condition.name, () => {
     });
 
     it('should map to source.truthy when evaluated to true', () => {
-        const conditionFn = condition(() => true);
-        const result = conditionFn[MapFnClassId.fn](source, ['toMap']);
+        let conditionFn = condition(() => true);
+        let result = conditionFn[MapFnClassId.fn](source, ['toMap']);
         expect(result).toEqual(source.toMap);
     });
 
     it('should map to source.truthy when evaluated to true regardless of defaultValue', () => {
-        const conditionFn = condition(() => true, 'defaultValue');
-        const result = conditionFn[MapFnClassId.fn](source, ['toMap']);
+        let conditionFn = condition(() => true, 'defaultValue');
+        let result = conditionFn[MapFnClassId.fn](source, ['toMap']);
         expect(result).toEqual(source.toMap);
     });
 
     it('should map to undefined when evaluated to false', () => {
-        const conditionFn = condition(() => false);
-        const result = conditionFn[MapFnClassId.fn](source, ['toMap']);
+        let conditionFn = condition(() => false);
+        let result = conditionFn[MapFnClassId.fn](source, ['toMap']);
         expect(result).toEqual(undefined);
     });
 
     it('should map to defaultValue when evaluated to false and defaultValue is provided', () => {
-        const conditionFn = condition(() => false, 'defaultValue');
-        const result = conditionFn[MapFnClassId.fn](source, ['toMap']);
+        let conditionFn = condition(() => false, 'defaultValue');
+        let result = conditionFn[MapFnClassId.fn](source, ['toMap']);
         expect(result).toEqual('defaultValue');
     });
 });
