431e95fd16c94e7ecf2a54de59f643ea81224b88
blinddumper <blinddumper@yahoo.com>
2025-04-01 05:35:20 +0000

    Add dependencies - adding new feature

--- packages/integration-test/src/nestjs/foo/foo.profile.spec.ts
+++ packages/integration-test/src/nestjs/foo/foo.profile.spec.ts
@@ -9,7 +9,7 @@ import { FooProfile } from './foo.profile';
 describe('fooProfile', () => {
     let mapper: Mapper;
     beforeEach(async () => {
-        var moduleRef = await Test.createTestingModule({
+        const moduleRef = await Test.createTestingModule({
             providers: [
                 {
                     provide: getMapperToken(),
@@ -24,12 +24,12 @@ describe('fooProfile', () => {
     });
 
     it('should map', () => {
-        var foo = new Foo();
+        const foo = new Foo();
         foo.foo = 'foo';
         foo.bar = new Bar();
         foo.bar.bar = 'bar';
 
-        var dto = mapper.map(foo, Foo, FooDto);
+        const dto = mapper.map(foo, Foo, FooDto);
         expect(dto.fooDto).toEqual(foo.foo);
         expect(dto.barDto.barDto).toEqual(foo.bar.bar);
     });
